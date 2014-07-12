afaa5c85f7be067895fc4e93a32c7e3a5fbe2b3d
Sammaye <sam.millman@googlemail.com>
2014-07-12 23:02:46 +0100

    Started adding API stuff

--- js/site.js
+++ js/site.js
@@ -1,3 +1,29 @@
 /**
  * 
  */
+$(function(){
+	$.get('https://api.github.com/users/Sammaye/repos', null, 'json')
+	.done(function(data){
+		console.log(data);
+		populateGrid(data);
+	});
+});
+
+function populateGrid(data){
+
+	var grid = $('.repo-grid');
+	grid.empty();
+	
+	$.each(data, function(){
+		var repo = $(this)[0];
+		grid.append(
+			$('<div/>', {'class' : 'col-sm-10'}).append(
+				$('<a/>', {href : $(this).html_url}).html($(this)[0].name)
+			)
+			.append(
+				$('<p/>').html($(this)[0].description)
+			)
+		);
+		
+	});
+}
